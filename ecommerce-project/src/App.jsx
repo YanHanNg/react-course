be38d3c9731c3068f32302fcae5111f9034e8d7b
Yan Han Ng <yanhanng89@gmail.com>
2026-06-18 09:35:50 +0800

    Move Checkout Page into React

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,4 +1,5 @@
 import { HomePage } from './pages/HomePage'
+import { CheckoutPage } from './pages/CheckoutPage';
 import { Routes, Route } from 'react-router';
 import './App.css'
 
@@ -7,9 +8,8 @@ function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
-      <Route path="checkout" element={<div>Test Checkout Page</div>} />
+      <Route path="checkout" element={<CheckoutPage />} />
     </Routes>
-
   )
 }
 
