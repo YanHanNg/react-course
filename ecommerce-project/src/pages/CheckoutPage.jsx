1598ec064d7c67e2628857a8fd036da8f3ddf7a6
Yan Han Ng <yanhanng89@gmail.com>
2026-06-22 09:14:32 +0800

    Move files, Switch to Common Header and Use Link

--- ecommerce-project/src/pages/CheckoutPage.jsx
+++ ecommerce-project/src/pages/CheckoutPage.jsx
@@ -1,30 +1,13 @@
 import './checkout-header.css';
 import './CheckoutPage.css';
+import { Header } from '../components/Header';
 
 export function CheckoutPage() {
     return (
         <>
             <title>Checkout</title>
 
-            <div class="checkout-header">
-                <div class="header-content">
-                    <div class="checkout-header-left-section">
-                        <a href="index.html">
-                            <img class="logo" src="images/logo.png" />
-                            <img class="mobile-logo" src="images/mobile-logo.png" />
-                        </a>
-                    </div>
-
-                    <div class="checkout-header-middle-section">
-                        Checkout (<a class="return-to-home-link"
-                            href="index.html">3 items</a>)
-                    </div>
-
-                    <div class="checkout-header-right-section">
-                        <img src="images/icons/checkout-lock-icon.png" />
-                    </div>
-                </div>
-            </div>
+            <Header />
 
             <div class="checkout-page">
                 <div class="page-title">Review your order</div>
